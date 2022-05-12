9a15b0737630faa4e46dfb8fdd9b907f05d60f9f
team19@stiglymsft.onmicrosoft.com <team19@stiglymsft.onmicrosoft.com>
2022-05-12 09:35:57 +0000

    Finished a function

--- FileMethods.cs
+++ FileMethods.cs
@@ -59,6 +59,7 @@ namespace TollBooth
                         stream.Position = 0;
                         // TODO 7: Asynchronously upload the blob from the memory stream.
                         // COMPLETE: await blob...;
+                        await blob.UploadFromStreamAsync(stream);
 
                         successful = true;
                     }
